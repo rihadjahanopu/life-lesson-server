04e057193fe398ee2cfa7a331eccb9de41093a76
Rihad Jahan Opu <96970767+rihadjahanopu@users.noreply.github.com>
2026-06-21 21:32:58 +0600

    feat: add stripe publishable key to config configuration

--- src/config/index.js
+++ src/config/index.js
@@ -14,6 +14,7 @@ const config = {
   },
   stripe: {
     secretKey: process.env.STRIPE_SECRET_KEY,
+    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
     priceId: process.env.STRIPE_PRICE_ID,
   },
